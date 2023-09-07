f50fb764f015903527d0c68e9d244ee038904793
IvxnSO <144163720+IvxnSO@users.noreply.github.com>
2023-09-07 16:07:03 -0300

    Add files via upload

---
+++ ivan/longitud.py
@@ -0,0 +1,7 @@
+def metro_pulgadas(metros):
+    formula = metros * 39.37
+    return f"el valor es {formula} pulgadas"
+
+def metro_pies(pies):
+    formula = pies * 3.28084
+    return f"el valor es {formula} pies"
---
+++ ivan/peso.py
@@ -0,0 +1,10 @@
+def kilogramos_gramos(kilos):
+    formula = kilos * 1000
+    return f"resultado {formula}"
+
+#iniciar git
+#git init
+
+#añadir archivos
+#git add .
+#git commit -m "mensaje"
